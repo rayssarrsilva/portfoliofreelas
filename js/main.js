bcec266616c378f804a2dcdc983bac411dc7737b
Rayssa <rayssarrsilva1@gmail.com>
2026-01-10 16:26:57 -0300

    manutenção, metodologia, planos, inicio

--- js/main.js
+++ js/main.js
@@ -171,7 +171,6 @@ document.addEventListener('DOMContentLoaded', function() {
       quotes[current].classList.remove('active');
       current = (current + 1) % quotes.length;
       quotes[current].classList.add('active');
-    }, 4000);
+    }, 3000);  
   }
-
 });
